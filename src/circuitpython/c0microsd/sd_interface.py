037562e3068e42656f0a526464eb3f17e1ba0b60
Apostolos Vailakis <a.vailakis@gmail.com>
2025-08-11 15:55:19 +0300

    Fix copyrights

--- src/circuitpython/c0microsd/sd_interface.py
+++ src/circuitpython/c0microsd/sd_interface.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (c) 2024, Signaloid.
+# Copyright (c) 2025, Signaloid.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
